8f820d6e3209561e5ddf5474c461a670e648e00b
SHYLA <sheilabernicew@gmail.com>
2019-04-30 08:12:50 +0300

    made changes on the page for it to be similar to app.component.ts

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import {Quote} from '../quote'
 
 export class QuoteComponent implements OnInit {
   quotes = [
-    new Quote(1,'Pintrest','Your will is more important than your skill', new Date(2018,3,14) ),
-    new Quote(2,'Bernice','Be better in life than in social media', new Date(2018,3,14)),
-    new Quote(3, 'Spongebob','You will never know the value of moment until it becomes a memory' ,new Date(2018,3,14)),
-    new Quote(4, 'Shyla','never quit code' , new Date(2018,3,14)),
+    new Quote(1,'Pintrest','Your will is more important than your skill', new Date(2016,3,14) ),
+    new Quote(2,'Bernice','Be better in life than in social media', new Date(2017,3,14)),
+    new Quote(3, 'Spongebob','You will never know the value of moment until it becomes a memory' ,new Date(2007,12,14)),
+    new Quote(4, 'Shyla','never quit code' , new Date(2008,3,24)),
   ]
 Quote(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription ;
